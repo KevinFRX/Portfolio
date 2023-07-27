6b2179fd49207b0d286aa4f559fd03ee9cc9ae5c
KevinFRX <xerokf13@gmail.com>
2023-07-27 20:35:52 -0300

    contact icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    css: [
+        '@fortawesome/fontawesome-free/css/all.min.css',
+    ],
+});
